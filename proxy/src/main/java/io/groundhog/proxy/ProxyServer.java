eda252b9e58e1ebb2d75c8c8525d569f5d366b6d
Danny Thomas <Danny.Thomas@blackboard.com>
2014-06-18 15:23:15 +0100

    Add client SSL configuration and switch to chaining for server connections. Other than some tests, this should do it for SSL support

--- proxy/src/main/java/io/groundhog/proxy/ProxyServer.java
+++ proxy/src/main/java/io/groundhog/proxy/ProxyServer.java
@@ -17,22 +17,30 @@
 
 package io.groundhog.proxy;
 
+import io.groundhog.base.HttpMessages;
 import io.groundhog.base.URIScheme;
 import io.groundhog.capture.CaptureWriter;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
 import com.google.common.net.HostAndPort;
+import com.google.common.net.HttpHeaders;
 import com.google.common.util.concurrent.AbstractIdleService;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
-import org.littleshoot.proxy.HttpFiltersSource;
-import org.littleshoot.proxy.HttpProxyServerBootstrap;
-import org.littleshoot.proxy.extras.SelfSignedMitmManager;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import org.littleshoot.proxy.*;
+import org.littleshoot.proxy.extras.SelfSignedSslEngineSource;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLEngine;
 import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Queue;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -59,13 +67,12 @@ public final class ProxyServer extends AbstractIdleService {
               @Named("listen.https") HostAndPort listenHttps,
               @Named("target.http") HostAndPort targetHttp,
               @Named("target.https") HostAndPort targetHttps) {
-    this(captureWriter, filterSourceFactory, listenHttp, targetHttp);
-    // Disabled for now, this is actually less straight forward than I'd hoped. LittleProxy only MITMs on CONNECT, not regular requests
-//    this.listenHttps = Optional.of(listenHttps);
-//    this.targetHttps = Optional.of(targetHttps);
-    // Keep the null tester happy
-    checkNotNull(listenHttps);
-    checkNotNull(targetHttps);
+    this.captureWriter = checkNotNull(captureWriter);
+    this.filterSourceFactory = checkNotNull(filterSourceFactory);
+    this.listenHttp = checkNotNull(listenHttp);
+    this.targetHttp = checkNotNull(targetHttp);
+    this.listenHttps = Optional.of(listenHttps);
+    this.targetHttps = Optional.of(targetHttps);
   }
 
   ProxyServer(CaptureWriter captureWriter, CaptureFilterSourceFactory filterSourceFactory, HostAndPort listen, HostAndPort target) {
@@ -90,9 +97,64 @@ public final class ProxyServer extends AbstractIdleService {
     HttpFiltersSource filterSource = filterSourceFactory.create(scheme, target);
     HttpProxyServerBootstrap bootstrap = DefaultHttpProxyServer.bootstrap().withAddress(toInetSocketAddress(listen)).withFiltersSource(filterSource);
     if (URIScheme.HTTPS == scheme) {
-      bootstrap.withManInTheMiddle(new SelfSignedMitmManager());
+      configureHttps(bootstrap);
     }
     bootstrap.start();
+  }
+
+  /**
+   * This is a little hacky, however it's the best way to achieve HTTPS support without modifying LittleProxy. The MITM
+   * manager only supports the CONNECT method, so we need to use a {@link ChainedProxy} to re-encrypt connections to
+   * the intended host.
+   */
+  private void configureHttps(HttpProxyServerBootstrap bootstrap) {
+    final SelfSignedSslEngineSource sslEngineSource = new SelfSignedSslEngineSource(true);
+
+    // Client to proxy connections
+    bootstrap.withSslEngineSource(sslEngineSource).withAuthenticateSslClients(false);
+
+    // Proxy to server connections
+    ChainedProxyManager chainedProxyManager = new ChainedProxyManager() {
+      @Override
+      public void lookupChainedProxies(final HttpRequest httpRequest, Queue<ChainedProxy> chainedProxies) {
+        chainedProxies.add(new ChainedProxyAdapter() {
+          @Override
+          public boolean requiresEncryption() {
+            return true;
+          }
+
+          @Override
+          public SSLEngine newSslEngine() {
+            return sslEngineSource.newSslEngine();
+          }
+
+          @Override
+          public InetSocketAddress getChainedProxyAddress() {
+            HostAndPort hostAndPort = HttpMessages.identifyHostAndPort(httpRequest);
+            return toInetSocketAddress(hostAndPort);
+          }
+
+          /**
+           * Filters the request to avoid the side-effects of chaining: removes the VIA header and ensures the URI is
+           * bare without scheme or hostname.
+           */
+          @Override
+          public void filterRequest(HttpObject httpObject) {
+            if (httpObject instanceof HttpRequest) {
+              HttpRequest proxyHttpRequest = (HttpRequest) httpObject;
+              try {
+                URL url = new URL(proxyHttpRequest.getUri());
+                proxyHttpRequest.setUri(url.getFile());
+              } catch (MalformedURLException e) {
+                throw Throwables.propagate(e);
+              }
+              proxyHttpRequest.headers().remove(HttpHeaders.VIA);
+            }
+          }
+        });
+      }
+    };
+    bootstrap.withChainProxyManager(chainedProxyManager);
   }
 
   private InetSocketAddress toInetSocketAddress(HostAndPort hostAndPort) {
